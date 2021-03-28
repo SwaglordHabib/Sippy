e392a92ebb0608dba3e8125e47ba46461aa1aed6
SwaglordHabib <maunel.seelig@outlook.de>
2021-03-28 12:57:52 +0200

    added new HEading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,15 +1,33 @@
+import { Stack } from '@fluentui/react';
 import React from 'react';
+import { Icon, IconHeadingButton } from '../Buttons/IconButton';
 import "./heading.css";
 
 export interface IHeadingsProps {
     Title: string;
+    OnClick?: () => void;
 }
 
 export const Heading: React.FunctionComponent<IHeadingsProps> = (props: React.PropsWithChildren<IHeadingsProps>) => {
 
     return (
         <div className={"heading"}>
-            <span className={"heading-title"}>{props.Title}</span>
+            <Stack horizontal>
+                <span className={"heading-title"}>{props.Title}</span>
+                {props.OnClick && <IconHeadingButton Icon={Icon.Plus} OnClick={props.OnClick} />}
+            </Stack>
+        </div>
+    );
+};
+
+export const BigHeading: React.FunctionComponent<IHeadingsProps> = (props: React.PropsWithChildren<IHeadingsProps>) => {
+
+    return (
+        <div className={"heading"}>
+            <Stack horizontal>
+                <span className={"heading-title-big"}>{props.Title}</span>
+                {props.OnClick && <IconHeadingButton Icon={Icon.Plus} OnClick={props.OnClick} />}
+            </Stack>
         </div>
     );
 };
